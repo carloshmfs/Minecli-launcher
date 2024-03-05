614010869babbeac7cb0942fac8fc65b8d85a186
robott11 <contato.robott@gmail.com>
2024-03-05 00:12:05 -0300

    feat: retrieve the tag for the auth api

--- minecli/Application.cpp
+++ minecli/Application.cpp
@@ -1,16 +1,18 @@
 #include "Application.h"
-#include "Auth.h"
+
+#include <cpr/cpr.h>
 
 #include <cstdlib>
 #include <iostream>
 #include <exception>
+#include <stdexcept>
+#include <regex>
 
 int Application::exec()
 {
     try {
         if (m_perform_auth) {
-            Auth auth;
-            auth.microsoft_login();
+            attempt_auth();
             return EXIT_SUCCESS;
         }
 
@@ -21,3 +23,26 @@ int Application::exec()
 
     return EXIT_SUCCESS;
 }
+
+void Application::retrieve_microsoft_tag()
+{
+    cpr::Response r = cpr::Get( cpr::Url { m_tag_endpoint } );
+
+    if (r.status_code != 200)
+        throw std::runtime_error("Unable to retrieve the sFTTag value. Failed the api request.");
+
+    std::smatch match;
+    if ( ! std::regex_search( r.text, match, std::regex("value=\"(.+?)\"") ) )
+        throw std::runtime_error("Unable to retrieve the sFTTag value. Value not found.");
+
+    if (match.size() < 1)
+        throw std::runtime_error("Unable to retrieve the sFTTag value. Empty Value.");
+    
+    m_tag = match[1];
+}
+
+void Application::attempt_auth()
+{ 
+    retrieve_microsoft_tag();
+}
+
--- minecli/Application.h
+++ minecli/Application.h
@@ -1,8 +1,20 @@
 #pragma once
 
-class Application {
+#include <string>
+
+class Application 
+{
+
 public:
     bool m_perform_auth = { false };
 
     int exec();
+    void attempt_auth();
+
+private:
+    std::string m_tag_endpoint = "https://login.live.com/oauth20_authorize.srf?client_id=000000004C12AE6F&redirect_uri=https://login.live.com/oauth20_desktop.srf&scope=service::user.auth.xboxlive.com::MBI_SSL&display=touch&response_type=token&locale=en";
+    std::string m_tag;
+
+    void retrieve_microsoft_tag();
+
 };
